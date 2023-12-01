2cb70a7c9c01f774eb9511475c6a1e5f67e3b8c8
Swiper <matinadekola33@gmail.com>
2023-12-01 17:18:14 +0100

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            three: 'three',
-        },
+  resolve: {
+    alias: {
+      three: 'three',
     },
+  }, 
 });
